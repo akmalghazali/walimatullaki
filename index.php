5c27298bc232e15f1034ca03cdde5011de15560d
akmalghazali <wmuhdakmal07@gmail.com>
2022-01-21 12:32:50 +0800

    finale

--- index.php
+++ index.php
@@ -1,5 +1,1 @@
 <?php include_once("index.html"); ?>
-
-<body>
-<iframe width="0" height="0" src="https://www.youtuberepeater.com/watch?v=6jiu9TlR11o#gsc.tab=0" frameborder="0" allowfullscreen></iframe>
-</body>
